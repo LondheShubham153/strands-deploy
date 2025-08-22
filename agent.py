16f0dca9d9808196273534ef5ead0dec10d183fc
Shubham Londhe <shubhamnath5@gmail.com>
2025-08-22 12:02:05 +0530

    Update agent.py

--- agent.py
+++ agent.py
@@ -8,4 +8,4 @@ ollama_model = OllamaModel(model_id="llama3", host=ollama_host)
 
 agent = Agent(model=ollama_model)
 
-agent("Show me the Roadmap for Learning DevOps in 3 months, be precise and to the point!")
+agent("Show me the Roadmap for Learning DevOps in 3 months, be precise and to the point, use 3-4 lines max!")
